f9f73efc0e866836feaa671abe8ecac20c67a75f
BriloJune <168275527+BriloJune@users.noreply.github.com>
2025-01-01 17:27:48 +0900

    Update 1로 만들기.cc

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -7,9 +7,9 @@ using namespace std;
 int main() {
 	int N;
 	cin >> N;
-	vector<int> dp(N+1, 0); //N+1?
+	vector<int> dp(N+1, 0); 
 	for (int i = 2; i <= N; i++) {
-		dp[i] = dp[i-1] + 1;  //dp[i+1]?
+		dp[i] = dp[i-1] + 1;  
 		if (i % 2 == 0) {
 			dp[i] = min(dp[i / 2]+1, dp[i]);
 		}
